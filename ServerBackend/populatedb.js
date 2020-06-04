45ef4f80829fa98ab90da091733b84b6961d2c9f
MiguelMarcelino <mac.marcelino@outlook.pt>
2020-06-04 23:35:00 +0100

    Comecado parte BackEnd para utilizadores

---
+++ ServerBackend/populatedb.js
@@ -0,0 +1,13 @@
+console.log('This script populates mock entities to the database for testing purposes' +
+    'populatedb mongodb://');
+
+var async = require('async');
+
+var mongoose = require('mongoose');
+var mongoDB = ;
+mongoose.connect(mongoDB, { useNewUrlParser: true, poolSize: 100 });
+mongoose.Promise = global.Promise;
+var db = mongoose.connection;
+db.on('error', console.error.bind(console, 'MongoDB connection error:'));
+
+
