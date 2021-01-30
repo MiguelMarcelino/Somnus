134700605376e19ec5de46da4b4ef257934737c9
MiguelMarcelino <mac.marcelino@outlook.pt>
2021-01-30 19:01:00 +0000

    Finished Articles API; Fixed Database connection

--- SomnusBackend/src/main/java/com/somnus/server/backend/articles/ArticleService.java
+++ SomnusBackend/src/main/java/com/somnus/server/backend/articles/ArticleService.java
@@ -1,19 +1,62 @@
 package com.somnus.server.backend.articles;
 
-import com.mongodb.client.MongoClient;
+import com.somnus.server.backend.articles.database.ArticleDatabaseConnection;
+import com.somnus.server.backend.articles.domain.Article;
 import com.somnus.server.backend.articles.domain.ArticleTopic;
 import com.somnus.server.backend.articles.dto.ArticleDto;
-import com.somnus.server.backend.config.DatabaseConnection;
 import org.springframework.stereotype.Service;
 
+import java.util.ArrayList;
 import java.util.List;
 
 @Service
 public class ArticleService {
 
-    public List<ArticleDto> getArticles(ArticleTopic articleTopic) {
-        // TODO
-        MongoClient mongoClient = DatabaseConnection.getMongoClient();
+    public ArticleDatabaseConnection articleDatabaseConnection;
+
+    public ArticleService() {
+        articleDatabaseConnection = new ArticleDatabaseConnection();
+    }
+
+    public List<ArticleDto> getAllArticles() {
+        List<ArticleDto> articleDtos = new ArrayList<>();
+        articleDatabaseConnection.getAllArticles().forEach(article -> articleDtos.add(
+                new ArticleDto(article.getArticleName(), article.getAuthorUserName(),
+                        article.getDescription(), article.getDatePublished(),
+                        article.getTopic().name, article.getContent())));
+        return articleDtos;
+    }
+
+    public List<ArticleDto> getArticlesOfTopic(String articleTopic) {
+        List<ArticleDto> articleDtos = new ArrayList<>();
+        articleDatabaseConnection.getArticlesMatchingTopic(articleTopic).forEach(article -> articleDtos.add(
+                new ArticleDto(article.getArticleName(), article.getAuthorUserName(),
+                        article.getDescription(), article.getDatePublished(),
+                        article.getTopic().name, article.getContent())));
+        return articleDtos;
+    }
+
+    public ArticleDto getArticle(String id) {
+        Article article = articleDatabaseConnection.getArticle(id);
+        return new ArticleDto(article.getArticleName(), article.getAuthorUserName(),
+                article.getDescription(), article.getDatePublished(),
+                article.getTopic().name, article.getContent());
+    }
+
+    public void createArticle(ArticleDto articleDto) {
+        Article article = new Article(articleDto.getArticleName(),
+                articleDto.getAuthorUserName(), articleDto.getDescription(),
+                ArticleTopic.valueOf(articleDto.getTopic()), articleDto.getContent());
+        articleDatabaseConnection.createArticle(article);
+    }
+
+    public void deleteArticle(String id) {
+        articleDatabaseConnection.deleteArticle(id);
+    }
+
+    public List<ArticleDto> searchArticles(String nameContent) {
+        // TODO: Check for any articles in the database containing the string
+        //  content within the name
         return null;
     }
 }
--- SomnusBackend/src/main/java/com/somnus/server/backend/exceptions/ErrorMessage.java
+++ SomnusBackend/src/main/java/com/somnus/server/backend/exceptions/ErrorMessage.java
@@ -1,9 +1,14 @@
 package com.somnus.server.backend.exceptions;
 
 public enum ErrorMessage {
+    NO_ARTICLE_FOUND("No article found"),
+    NO_ARTICLES_TO_DELETE("There are no articles that can be deleted"),
+    ARTICLE_INSERT_FAILED("Failed on inserting a new article"),
+
     ACCESS_DENIED("You do not have permission to access the requested resource"),
     BAD_REQUEST("Malformed Request"),
-    INTERNAL_SERVER_ERROR("Server error");
+    INTERNAL_SERVER_ERROR("Server error"),
+    DATABASE_CONNECTION_ERROR("Error connecting to database");
 
     public final String message;
 
