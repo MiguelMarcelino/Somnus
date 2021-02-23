0989126a036f4bd173215931a6aad5fec23dae0b
MiguelMarcelino <mac.marcelino@outlook.pt>
2021-02-23 19:18:46 +0000

    Corrected bug in articles section

--- SomnusBackend/src/main/java/com/somnus/server/backend/auth/AuthenticationSecurity.java
+++ SomnusBackend/src/main/java/com/somnus/server/backend/auth/AuthenticationSecurity.java
@@ -2,7 +2,6 @@ package com.somnus.server.backend.auth;
 
 import com.somnus.server.backend.auth.firebase.FirebaseAuthenticationProvider;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.Ordered;
