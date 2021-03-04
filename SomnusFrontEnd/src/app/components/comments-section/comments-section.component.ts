526c3c3fd4ca3994d0b0d363959b52ab6acca8f3
MiguelMarcelino <mac.marcelino@outlook.pt>
2021-03-04 19:37:53 +0000

    Added template for comments scetion

---
+++ SomnusFrontEnd/src/app/components/comments-section/comments-section.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-comments-section',
+  templateUrl: './comments-section.component.html',
+  styleUrls: ['./comments-section.component.scss']
+})
+export class CommentsSectionComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
