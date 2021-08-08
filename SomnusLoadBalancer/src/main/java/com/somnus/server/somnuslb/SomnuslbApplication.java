fa8c38654d04ab3a3e78e23d1e07338b52cf84c7
MiguelMarcelino <mac.marcelino@outlook.pt>
2021-08-08 19:27:01 +0100

    Added new files for Load Balancer Project

---
+++ SomnusLoadBalancer/src/main/java/com/somnus/server/somnuslb/SomnuslbApplication.java
@@ -0,0 +1,13 @@
+package com.somnus.server.somnuslb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SomnuslbApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SomnuslbApplication.class, args);
+	}
+
+}
---
+++ SomnusLoadBalancer/src/test/java/com/somnus/server/somnuslb/SomnuslbApplicationTests.java
@@ -0,0 +1,13 @@
+package com.somnus.server.somnuslb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SomnuslbApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
